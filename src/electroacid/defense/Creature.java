491acab601f27dedb285aa2e348c41a35a665865
tom.dubin <tom.dubin@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-03-08 20:51:54 +0000

    Creature won't be abstract class. Add Rokon into the classpath

--- src/electroacid/defense/Creature.java
+++ src/electroacid/defense/Creature.java
@@ -1,6 +1,6 @@
 package electroacid.defense;
 
-public abstract class Creature {
+public  class Creature {
 
 		private Element element; 
 		private int life;
@@ -8,6 +8,15 @@ public abstract class Creature {
 		private int fireRate;
 		private int cost;
 		private boolean fly;
+		
+		public Creature(Element e,int l,int s, int fR, int c, boolean f){
+			this.element = e;
+			this.life = l;
+			this.speed = s;
+			this.fireRate = fR;
+			this.cost = c;
+			this.fly = f;			
+		}
 		
 		public Element getElement(){
 			return this.element;
@@ -27,8 +36,28 @@ public abstract class Creature {
 		public boolean getFly(){
 			return this.fly;
 		}
+		public Element setElement(Element e){
+			this.element = e;
+			return this.element;
+		}
 		public int setLife(int nb){
 			this.life = nb;
 			return this.life;
+		}
+		public int setSpeed(int nb){
+			this.speed = nb;
+			return this.speed;
+		}
+		public int setFireRate(int nb){
+			this.fireRate = nb;
+			return this.fireRate;
+		}
+		public int setCost(int nb){
+			this.cost = nb;
+			return this.cost;
+		}
+		public boolean setFly(boolean f){
+			this.fly = f;
+			return this.fly;
 		}
 }
