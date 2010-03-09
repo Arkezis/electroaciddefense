e3fff819021fbe40eba0c0bd1108068e20d715c9
tom.dubin <tom.dubin@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-03-09 18:22:32 +0000

    File where the game will be run

---
+++ src/electroacid/defense/Play.java
@@ -0,0 +1,14 @@
+package electroacid.defense;
+
+import android.R;
+import android.app.*;
+import android.os.*;
+
+public class Play extends Activity{
+
+	public void onCreate(Bundle me){
+		super.onCreate(me);
+		setContentView(R.layout.main);
+	}
+	
+}
