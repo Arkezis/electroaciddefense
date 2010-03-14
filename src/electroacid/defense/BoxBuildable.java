0de9fee77a98f7919ee5e4d245a8dd0eb3623008
tom.dubin <tom.dubin@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-03-14 21:28:56 +0000

    Adding the reference to the tower !

--- src/electroacid/defense/BoxBuildable.java
+++ src/electroacid/defense/BoxBuildable.java
@@ -8,10 +8,23 @@ public class BoxBuildable extends Box {
 
 
 		//private SubstanceBuildable sub; The substance of the box
-
+		private Tower tower; 
 	
 	public BoxBuildable(Texture t){
-
 		this.createBoxBuildable(t);
+		tower=null;
 	}
+	public BoxBuildable(Texture t,Tower tow){
+		this.createBoxBuildable(t);
+		tower=tow;
+	}
+
+	public Tower getTower(){
+		return tower;
+	}
+	
+	public void setTower(Tower t){
+		tower = t;
+	}
+	
 }
