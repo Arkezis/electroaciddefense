f0a7df07d97bfb3efb4e9e5aa0b223905a1ecfba
cilheo <cilheo@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-04-06 14:02:45 +0000

    modification of boxPath

--- trunk/src/electroacid/defense/box/BoxPath.java
+++ trunk/src/electroacid/defense/box/BoxPath.java
@@ -1,8 +1,38 @@
 package electroacid.defense.box;
 
+import java.util.LinkedList;
+
+import electroacid.defense.Creature;
+
 public class BoxPath extends Box {
+
+	private LinkedList<Creature> listCreature;
+	
+	private BoxPath nextPath;
+	
+	
+	/**
+	 * @return the nextPath
+	 */
+	public BoxPath getNextPath() {
+		return nextPath;
+	}
+
+	/**
+	 * @param nextPath the nextPath to set
+	 */
+	public void setNextPath(BoxPath nextPath) {
+		this.nextPath = nextPath;
+	}
 
 	public BoxPath(int _x, int _y, int _width, int _height) {
 		super(_x,_y,_width,_height);
+		this.listCreature = new LinkedList<Creature>();
 	}
+	
+	public void addCreature(Creature creature){
+		this.listCreature.add(creature);
+	}
+	
+	
 }
