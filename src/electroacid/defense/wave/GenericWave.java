43d2e488798efa42ee92767f72dc66cbe2ed0d57
cilheo <cilheo@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-04-12 15:16:32 +0000

    add comment and little refactoring

--- src/electroacid/defense/wave/GenericWave.java
+++ src/electroacid/defense/wave/GenericWave.java
@@ -1,89 +1,80 @@
 package electroacid.defense.wave;
 
 import java.util.LinkedList;
-
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
 import utils.XmlUtil;
 import android.content.Context;
-import android.util.Log;
-
 import com.android.angle.AngleSpriteLayout;
 import com.android.angle.AngleSurfaceView;
-
 import electroacid.defense.Creature;
 import electroacid.defense.R;
 import electroacid.defense.enums.Element;
 
 public class GenericWave {
 
+	/** Waves' creature */
 	private LinkedList<Wave> listWave = new LinkedList<Wave>();
+	/** layouts' list, contain all possibility of creature layout*/
 	private LinkedList<AngleSpriteLayout> listLayout;
-	
-	public void build(final Context context,final int xmlResourceId) throws Exception{
-		Document waveXml = XmlUtil.getDocumentFromResource(context, xmlResourceId);
-
-		NodeList listNodeWave = waveXml.getDocumentElement().getElementsByTagName("wave");
-		
-		for (int i=0;i<listNodeWave.getLength();i++){
-			
-			NodeList listCreature = listNodeWave.item(i).getChildNodes();
-			
-			Wave wave = new Wave();
-			this.listWave.add(wave);	
-			for (int j=0;j<listCreature.getLength();j++){
-				Node creature = listCreature.item(j);
-				if (creature.getNodeName().equalsIgnoreCase("creature")){
-				
-				
-				Element element = Element.getElement(XmlUtil.getAttributeFromNode(creature, "element"));
-				int life = XmlUtil.getAttributeIntFromNode(creature, "life");
-				int speed = XmlUtil.getAttributeIntFromNode(creature, "speed");
-				int fireRate = XmlUtil.getAttributeIntFromNode(creature, "fireRate");
-				int rewardValue = XmlUtil.getAttributeIntFromNode(creature, "rewardValue");
-				boolean fly = XmlUtil.getAttributeBooleanFromNode(creature, "fly");
-				int idTexture = XmlUtil.getAttributeIntFromNode(creature, "textureId");
-				int number = XmlUtil.getAttributeIntFromNode(creature, "number");
-				
-				for (int num=0;num<number;num++){
-					Creature creat = new Creature(element,
-							life,
-							speed,
-							fireRate,
-							rewardValue,
-							fly,
-							this.listLayout.get(idTexture));
-						wave.addCreature(creat);				
-				}
-				}
-			}
-		}
-	}
-
 
 	/**
-	 * @return the listWave
+	 * Constructor of genericWave
+	 * init all layout
+	 * @param mGLSurfaceView
 	 */
-	public LinkedList<Wave> getListWave() {
-		return listWave;
-	}
-
-
-	/**
-	 * @param listWave the listWave to set
-	 */
-	public void setListWave(LinkedList<Wave> listWave) {
-		this.listWave = listWave;
-	}
-
-
 	public GenericWave(AngleSurfaceView mGLSurfaceView) {
 		this.listLayout = new LinkedList<AngleSpriteLayout>();
 		this.listLayout.add(new AngleSpriteLayout(
 				mGLSurfaceView,32,32,R.drawable.creature1));
 	}
 	
-	
+	/**
+	 * Read the xml file and create waves and creatures
+	 * @param context use for read the xml file
+	 * @param xmlResourceId id of the xml file
+	 * @throws Exception
+	 */
+	public void build(final Context context,final int xmlResourceId) throws Exception{
+		Document waveXml = XmlUtil.getDocumentFromResource(context, xmlResourceId);
+		NodeList listNodeWave = waveXml.getDocumentElement().getElementsByTagName("wave");
+		for (int i=0;i<listNodeWave.getLength();i++){
+			NodeList listCreature = listNodeWave.item(i).getChildNodes();
+			Wave wave = new Wave();
+			this.listWave.add(wave);	
+			for (int j=0;j<listCreature.getLength();j++){
+				Node creature = listCreature.item(j);
+				if (creature.getNodeName().equalsIgnoreCase("creature")){
+
+					Element element = Element.getElement(XmlUtil.getAttributeFromNode(creature, "element"));
+					int life = XmlUtil.getAttributeIntFromNode(creature, "life");
+					int speed = XmlUtil.getAttributeIntFromNode(creature, "speed");
+					int fireRate = XmlUtil.getAttributeIntFromNode(creature, "fireRate");
+					int rewardValue = XmlUtil.getAttributeIntFromNode(creature, "rewardValue");
+					boolean fly = XmlUtil.getAttributeBooleanFromNode(creature, "fly");
+					int idTexture = XmlUtil.getAttributeIntFromNode(creature, "textureId");
+					int number = XmlUtil.getAttributeIntFromNode(creature, "number");
+
+					for (int num=0;num<number;num++){
+						Creature creat = new Creature(element,
+								life,
+								speed,
+								fireRate,
+								rewardValue,
+								fly,
+								this.listLayout.get(idTexture));
+						wave.addCreature(creat);				
+					}
+				}
+			}
+		}
+	}
+
+	/** @return the listWave */
+	public LinkedList<Wave> getListWave() {return listWave;}
+
+	/** @param listWave the listWave to set */
+	public void setListWave(LinkedList<Wave> listWave) {this.listWave = listWave;}
+
 }
--- src/electroacid/defense/wave/Wave.java
+++ src/electroacid/defense/wave/Wave.java
@@ -1,69 +1,78 @@
 package electroacid.defense.wave;
 
 import java.util.LinkedList;
-
-import android.util.Log;
-
 import com.android.angle.AngleObject;
 import electroacid.defense.Creature;
 import electroacid.defense.box.BoxPath;
 
+/**
+ * it's a wave of creatures
+ * @author cilheo
+ * @version 1.0b
+ */
 public class Wave extends AngleObject {
 
+	/** creatures' list */
 	private LinkedList<Creature> listCreature ;
+
+	/** wave is start or not */
 	private boolean start = false;
-	private float timeBeetweenCreature = 2;
+
+	/** time between creatures' start */
+	private float timeBetweenCreature = 2;
+
+	/** container of creature for print */
 	private AngleObject og;
+
+	/** first box for the creatures' path */
 	private BoxPath debut;
+
+	/** position in the list of the actual creature */
 	private int actualCreature = 0;
-	
-	
+
+	/**
+	 * Constructor of wave
+	 * initialise the creatures' list
+	 */
 	public Wave(){
 		this.listCreature = new LinkedList<Creature>();
 	}
-	
-	/**
-	 * @return the listCreature
-	 */
-	public LinkedList<Creature> getListCreature() {
-		return listCreature;
-	}
 
 	/**
-	 * @param listCreature the listCreature to set
+	 * Add a creature to the list
+	 * @param creature creature to add
+	 * @return true if the add is correct
 	 */
-	public void setListCreature(LinkedList<Creature> listCreature) {
-		this.listCreature = listCreature;
-	}
-
 	public boolean addCreature(Creature creature){
 		return this.listCreature.add(creature);
 	}
-	
+
+	/**
+	 * Start the wave
+	 * @param ogCreature container for creature
+	 * @param boxpath first boxPath
+	 */
 	public void start(AngleObject ogCreature, BoxPath boxpath){
 		this.debut = boxpath;
 		this.og = ogCreature;
 		this.start = true;
 	}
-	
-	public void show(int numberOfCreature){
-		this.listCreature.get(numberOfCreature).show();
-	}
 
+	/**
+	 * action during a step
+	 */
 	public void step(float secondsElapsed){
 		if (this.start){
-			this.timeBeetweenCreature-=secondsElapsed;
-			if (this.timeBeetweenCreature<=0){
-				this.timeBeetweenCreature=2;
+			this.timeBetweenCreature-=secondsElapsed;
+			if (this.timeBetweenCreature<=0){
+				this.timeBetweenCreature=2;
 				if (this.actualCreature<this.listCreature.size()){
 					this.listCreature.get(this.actualCreature).start(
 							this.og,
 							this.debut);
 					this.actualCreature++;
 				}
-				
 			}
-			
 		}
 		super.step(secondsElapsed);
 	}
