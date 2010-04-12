8f730a46a0944cadf0eab4ce205085e30bb99b97
cilheo <cilheo@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-04-12 14:48:40 +0000

    add comment and little refactoring

--- src/electroacid/defense/box/Box.java
+++ src/electroacid/defense/box/Box.java
@@ -1,13 +1,32 @@
 package electroacid.defense.box;
 
+/**
+ * It's a box on the game map
+ * @author cilheo
+ * @version 1.0b
+ */
 public abstract class Box {
 
+	
+	/** The x coordinate on the map */
 	protected int x;
+	
+	/** The y coordinate on the map */
 	protected int y;
 
+	/** Width of the box */
 	protected int width;
+	
+	/** Height of the box */
 	protected int height;
 
+	/**
+	 * Constructor of the box
+	 * @param _x the x coordinate on the map
+	 * @param _y the y coordinate on the map
+	 * @param _width widht of the box
+	 * @param _height height of the box
+	 */
 	public Box(int _x, int _y, int _width, int _height) {
 		this.x = _x;
 		this.y = _y;
@@ -15,59 +34,28 @@ public abstract class Box {
 		this.height = _height;
 	}
 
-	/**
-	 * @return the x
-	 */
-	public int getX() {
-		return this.x;
-	}
+	/** @return the x */
+	public int getX() {return x;}
 
-	/**
-	 * @param x the x to set
-	 */
-	public void setX(int x) {
-		this.x = x;
-	}
+	/** @param x the x to set */
+	public void setX(int _x) {this.x = _x;}
 
-	/**
-	 * @return the y
-	 */
-	public int getY() {
-		return this.y;
-	}
-	/**
-	 * @param y the y to set
-	 */
-	public void setY(int y) {
-		this.y = y;
-	}
+	/** @return the y */
+	public int getY() {return y;}
 
-	/**
-	 * @return the width
-	 */
-	public int getWidth() {
-		return this.width;
-	}
+	/** @param y the y to set */
+	public void setY(int _y) {this.y = _y;}
 
-	/**
-	 * @param width the width to set
-	 */
-	public void setWidth(int width) {
-		this.width = width;
-	}
+	/** @return the width */
+	public int getWidth() {return width;}
 
-	/**
-	 * @return the height
-	 */
-	public int getHeight() {
-		return this.height;
-	}
+	/** @param width the width to set */
+	public void setWidth(int _width) {this.width = _width;}
 
-	/**
-	 * @param height the height to set
-	 */
-	public void setHeight(int height) {
-		this.height = height;
-	}
+	/** @return the height */
+	public int getHeight() {return height;}
+
+	/** @param height the height to set */
+	public void setHeight(int _height) {this.height = _height;}
 
 }
--- src/electroacid/defense/box/BoxBuildable.java
+++ src/electroacid/defense/box/BoxBuildable.java
@@ -3,37 +3,61 @@ package electroacid.defense.box;
 import electroacid.defense.Game;
 import electroacid.defense.Tower;
 
+/**
+ * It's a box which can host a tower
+ * @author cilheo
+ * @version 1.0b
+ */
 public class BoxBuildable extends Box {
 
+	/** A tower host by the box */
 	private Tower tower; 
 
+	/**
+	 * Constructor of the boxBuildable without a tower
+	 * @param _x the x coordinate on the map
+	 * @param _y the y coordinate on the map
+	 * @param _width widht of the box
+	 * @param _height height of the box
+	 */
 	public BoxBuildable(int _x, int _y, int _width, int _height) {
 		super(_x,_y,_width,_height);
 	}
 	
+	/**
+	 * Constructor of the boxBuildable with a tower
+	 * @param _x the x coordinate on the map
+	 * @param _y the y coordinate on the map
+	 * @param _width widht of the box
+	 * @param _height height of the box
+	 * @param _tower tower host by the box
+	 */
 	public BoxBuildable(int _x, int _y, int _width, int _height,Tower _tower) {
 		this(_x,_y,_width,_height);
 		this.tower=_tower;
 	}
 	
-	public Tower getTower(){
-		return this.tower;
-	}
-	
+	/**
+	 * Add a tower and change the actual tower host by the box
+	 * @param _tower the tower to add or change
+	 * @param game parametre of the game
+	 * @return false if tower is null or tower is too much expensive
+	 */
 	public boolean changeTower(Tower _tower,Game game){
 		this.tower = _tower;
 		if (this.tower != null) {
-			if(game.getMoney() > this.getTower().getCost()){
+			if(game.getMoney() > this.tower.getCost()){
 				this.tower.changePosition(this.y+this.width/2,this.x+this.height/2);
-				game.setMoney(game.getMoney()-this.getTower().getCost());
+				game.addMoney(-this.tower.getCost());
 				return true;
 			}
 		}
 		return false;
 	}
 	
-	public void removeTower() {
-			this.tower = null;
-	}
-	
+	/** Remove the tower of the box */
+	public void removeTower() {this.tower = null;}
+
+	/** @return the tower */
+	public Tower getTower() {return tower;}
 }
