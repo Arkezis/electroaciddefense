a432b002ad7d38624fac6c2518e359621b96a1e9
cilheo <cilheo@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-04-15 15:39:11 +0000

    cleaning for the 100th revision and version 1.0b

--- trunk/src/electroacid/defense/gui/MenuNewTower.java
+++ trunk/src/electroacid/defense/gui/MenuNewTower.java
@@ -17,7 +17,6 @@ public class MenuNewTower {
 	public AngleString t_infosTowerTitle,t_infosTowerElement;
 	public AngleString t_infosTowerCanTargetFly, t_infosTowerDamage;
 	public AngleSpriteLayout bGoLayout;
-	//public AngleSprite bnewTower1,bnewTower2,bGo;
 	public AngleSprite bGo;
 	
 	/**
@@ -31,18 +30,12 @@ public class MenuNewTower {
 		this.t_infosTowerElement = new AngleString(font,"",160,440,AngleString.aCenter);		
 		this.t_infosTowerDamage = new AngleString(font,"",160,450,AngleString.aCenter);
 		this.t_infosTowerCanTargetFly = new AngleString(font,"",160,460,AngleString.aCenter);		
-		
-		
-		
+
 		listTower.get(0).changePosition(0, 416);
 		listTower.get(0).getSprite().mAlpha = 0;
-		//this.bnewTower1 = new AngleSprite(new AngleSpriteLayout(mGLSurfaceView,32,32,R.drawable.tilemap,0,128,32,32));
-		//this.bnewTower1.mPosition.set(16, 432); 
 
 		listTower.get(1).changePosition(0, 448);
 		listTower.get(1).getSprite().mAlpha = 0;
-		//this.bnewTower2 = new AngleSprite(new AngleSpriteLayout(mGLSurfaceView,32,32,R.drawable.tilemap,32,128,32,32));
-		//this.bnewTower2.mPosition.set(16, 464); 
 				
 		this.bGoLayout = new AngleSpriteLayout(mGLSurfaceView,64,32,R.drawable.tilemap,128,160,64,32);
 		this.bGo = new AngleSprite(this.bGoLayout);
@@ -50,15 +43,11 @@ public class MenuNewTower {
 		
 		this.t_infosTowerTitle.mAlpha = 0;
 		
-		//this.bnewTower1.mAlpha = 0;
-		//this.bnewTower2.mAlpha = 0;
 		this.bGo.mAlpha=0;
 		this.t_infosTowerElement.mAlpha = 0;
 		this.t_infosTowerDamage.mAlpha = 0;
 		this.t_infosTowerCanTargetFly.mAlpha = 0;
 		mGLSurfaceView.addObject(this.t_infosTowerTitle);
-		//mGLSurfaceView.addObject(bnewTower1);
-		//mGLSurfaceView.addObject(bnewTower2);
 		mGLSurfaceView.addObject(bGo);
 		mGLSurfaceView.addObject(t_infosTowerElement);
 		mGLSurfaceView.addObject(t_infosTowerDamage);
@@ -86,11 +75,7 @@ public class MenuNewTower {
 	 */
 	public void hide(AngleSurfaceView mGLSurfaceView,LinkedList<Tower> listTower){
 		this.t_infosTowerTitle.mAlpha = 0;
-		//this.bnewTower1.mAlpha = 0;
-		//this.bnewTower2.mAlpha = 0;
-		for (int i=0;i<listTower.size();i++){
-			listTower.get(i).getSprite().mAlpha=0;
-		}
+		for (int i=0;i<listTower.size();i++) listTower.get(i).getSprite().mAlpha=0;
 	}
 	
 	/**
@@ -99,11 +84,7 @@ public class MenuNewTower {
 	 */
 	public void show(AngleSurfaceView mGLSurfaceView,LinkedList<Tower> listTower){
 		this.t_infosTowerTitle.mAlpha = 1;
-		//this.bnewTower1.mAlpha = 1;
-		//this.bnewTower2.mAlpha = 1;
-		for (int i=0;i<listTower.size();i++){
-			listTower.get(i).getSprite().mAlpha=1;
-		}
+		for (int i=0;i<listTower.size();i++) listTower.get(i).getSprite().mAlpha=1;
 		this.hideValidateTower(mGLSurfaceView); // if you want to show the selection, the validation must be hidden (for the moment !)
 	}
 	
