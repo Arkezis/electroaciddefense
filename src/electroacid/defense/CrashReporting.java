0d21a96567165e4ad43b4222f29c63a5b69b3455
cilheo <cilheo@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-06-11 15:12:12 +0000

    add a auto-crashReport

---
+++ src/electroacid/defense/CrashReporting.java
@@ -0,0 +1,12 @@
+package electroacid.defense;
+
+import org.acra.CrashReportingApplication;
+
+public class CrashReporting extends CrashReportingApplication {
+
+	@Override
+	public String getFormId() {
+		return "dFo4a0VCcnJyWFh4SXhvSThUdURMOUE6MQ";
+	}
+
+}
