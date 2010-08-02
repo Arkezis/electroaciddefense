85e66c0d6d7e259da0c0493a2a7e1c5f0b9d8002
tom.dubin <tom.dubin@c12128e4-2244-11df-b8aa-09cf7dbb0c5d>
2010-08-02 19:09:53 +0000

    HowTo finished Improving the wave games (thanks to Kevin D.)

---
+++ src/electroacid/defense/HowTo.java
@@ -0,0 +1,17 @@
+package electroacid.defense;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HowTo extends Activity{
+	
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.howto);
+
+		
+	}
+	
+
+}
